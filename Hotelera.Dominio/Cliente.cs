47b2f7d54e0425f667b41553c46d6d6d15d070ae
Sergio André Pérez Chávez <sergio_perezch@hotmail.com>
2016-05-06 08:48:28 -0500

    Comentando

--- Hotelera.Dominio/Cliente.cs
+++ Hotelera.Dominio/Cliente.cs
@@ -15,16 +15,16 @@ namespace Hotelera.Dominio
         /// <summary>
         /// Éste metodo nos servirá para agregar a los Clientes
         /// </summary>
-        /// <param name="id_cli"></param>
-        /// <param name="estado_cli"></param>
-        /// <param name="nombre_cli"></param>
-        /// <param name="apel_cli"></param>
-        /// <param name="gen_cli"></param>
-        /// <param name="tipoDoc_cli"></param>
-        /// <param name="nrDoc_cli"></param>
-        /// <param name="tel_cli"></param>
-        /// <param name="email_cli"></param>
-        /// <param name="dir_cli"></param>
+        /// <param name="id_cli">Id del cliente</param>
+        /// <param name="estado_cli">Estado del Cliente </param>
+        /// <param name="nombre_cli">Nombre del Cliente</param>
+        /// <param name="apel_cli">Apellido del Cliente</param>
+        /// <param name="gen_cli">Genero del Cliente</param>
+        /// <param name="tipoDoc_cli">TipDocumento del Cliente</param>
+        /// <param name="nrDoc_cli">NrDocumento del Cliente</param>
+        /// <param name="tel_cli">Telefono del Cliente</param>
+        /// <param name="email_cli">Email del Cliente</param>
+        /// <param name="dir_cli">Direccion del Cliente</param>
         public Cliente(int id_cli,string estado_cli, string nombre_cli, string apel_cli, string gen_cli, string tipoDoc_cli,
                                                             string nrDoc_cli, string tel_cli, string email_cli, string dir_cli) 
                                                             :base( nombre_cli, apel_cli, gen_cli, tipoDoc_cli,
@@ -36,13 +36,13 @@ namespace Hotelera.Dominio
         /// <summary>
         ///  Para Actualizar la informacioon de un Cliente
         /// </summary>
-        /// <param name="id_cli"></param>
-        /// <param name="nomb_cli"></param>
-        /// <param name="ape_cli"></param>
-        /// <param name="doc_cli"></param>
-        /// <param name="cel_cli"></param>
-        /// <param name="email_cli"></param>
-        /// <param name="estado_cli"></param>
+        /// <param name="id_cli">Id del cliente</param>
+        /// <param name="nomb_cli">Nombre del Cliente</param>
+        /// <param name="ape_cli">Apellido del Cliente</param>
+        /// <param name="doc_cli">Documento del Cliente</param>
+        /// <param name="cel_cli">Celular Cliente</param>
+        /// <param name="email_cli">Email del Cliente</param>
+        /// <param name="estado_cli">Estado del Cliente</param>
         public void Actualizar(int id_cli, string nomb_cli, string ape_cli, int doc_cli, string cel_cli, string email_cli, string estado_cli)
         {
             ID_Cliente = id_cli;
