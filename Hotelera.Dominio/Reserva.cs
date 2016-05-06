58c419a69a102c8da159834849246905ff0ea6fc
Sergio André Pérez Chávez <sergio_perezch@hotmail.com>
2016-05-06 10:06:49 -0500

    Comentando

--- Hotelera.Dominio/Reserva.cs
+++ Hotelera.Dominio/Reserva.cs
@@ -8,6 +8,9 @@ namespace Hotelera.Dominio
 {
     public class Reserva
     {
+        /// <summary>
+        ///  Parametros de Reserva
+        /// </summary>
         public int ID_Reserva { get; private set; }
         public virtual Habitacion ID_Habitacion { get; private set; }
         public virtual Cliente ID_Cliente { get; private set; }
@@ -19,6 +22,20 @@ namespace Hotelera.Dominio
         public DateTime Fecha_IngresoReserva { get; private set; }
         public DateTime Fecha_SalidaReserva { get; private set; }
         public virtual Agregados ID_Agregados { get; private set; }
+        
+        /// <summary>
+        ///  Metodo para Aperturar una Reserva
+        /// </summary>
+        /// <param name="id_hab">Id de habitacion</param>
+        /// <param name="id_client">Id del Cliente</param>
+        /// <param name="id_usu">Id de usuario</param>
+        /// <param name="id_tipreserv">id tipo de servicio</param>
+        /// <param name="id_medioreserv">id medio por cual se hace el servicio</param>
+        /// <param name="estado_reserv">Estado de la Reserva</param>
+        /// <param name="fecha_reserv">fecha de reserva</param>
+        /// <param name="fecha_ingreso">fecha de ingreso</param>
+        /// <param name="fecha_salida">fecha de salida/param>
+        /// <param name="id_agregad">Id Agregados</param>
         
         public virtual Reserva Aperturar(Habitacion id_hab, Cliente id_client,Usuario id_usu,TipoReserva id_tipreserv, MedioReserva id_medioreserv,string estado_reserv,DateTime fecha_reserv, DateTime fecha_ingreso, DateTime fecha_salida, Agregados id_agregad)
         {
@@ -36,6 +53,20 @@ namespace Hotelera.Dominio
                 ID_Agregados = id_agregad
             };
         }
+        
+        /// <summary>
+        ///  Metodo para actualizar una Reserva
+        /// </summary>
+        /// <param name="id_hab">Id de habitacion</param>
+        /// <param name="id_client">Id del Cliente</param>
+        /// <param name="id_usu">Id de usuario</param>
+        /// <param name="id_tipreserv">id tipo de servicio</param>
+        /// <param name="id_medioreserv">id medio por cual se hace el servicio</param>
+        /// <param name="estado_reserv">Estado de la Reserva</param>
+        /// <param name="fecha_reserv">fecha de reserva</param>
+        /// <param name="fecha_ingreso">fecha de ingreso</param>
+        /// <param name="fecha_salida">fecha de salida/param>
+        /// <param name="id_agregad">Id Agregados</param>
         public void Actualizar(Habitacion id_hab, Cliente id_client, Usuario id_usu, TipoReserva id_tipreserv, MedioReserva id_medioreserv, string estado_reserv, DateTime fecha_reserv, DateTime fecha_ingreso, DateTime fecha_salida, Agregados id_agregad)
         {
             ID_Habitacion = id_hab;
@@ -49,6 +80,10 @@ namespace Hotelera.Dominio
             Fecha_SalidaReserva = fecha_salida;
             ID_Agregados = id_agregad;
         }
+        
+        /// <summary>
+        ///  Metodo para Anular una Reserva
+        /// </summary>
         public void Anular()
         {
             Estado_Reserva = "ELIMINADO";            
