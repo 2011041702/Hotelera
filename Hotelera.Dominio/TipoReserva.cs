6dd04341b40ebb31dd16db54b2cd57111d65d5af
Sergio André Pérez Chávez <sergio_perezch@hotmail.com>
2016-05-06 08:55:22 -0500

    Comentando

--- Hotelera.Dominio/TipoReserva.cs
+++ Hotelera.Dominio/TipoReserva.cs
@@ -8,9 +8,16 @@ namespace Hotelera.Dominio
 {
     public class TipoReserva
     {
+        /// <summary>
+        ///  Parametros de Tipo de Reserva
+        /// </summary>
         public int ID_TipoReserva { get; private set; }
         public string Nombre_TipoReserva { get; private set; }
-
+        
+        /// <summary>
+        ///  Metodo para registrar un Tipo de Reserva
+        /// </summary>
+        /// <param name="nomb_tiporeserva">Nombre del tipo de reserva</param>
         public static TipoReserva Registrar(string nomb_tiporeserva)
         {
             return new TipoReserva()
@@ -18,11 +25,21 @@ namespace Hotelera.Dominio
                 Nombre_TipoReserva = nomb_tiporeserva
             };
         }
+        
+        /// <summary>
+        ///  Metodo para actualizar un Tipo de Reserva
+        /// </summary>
+        /// <param name="id_tiporeserva">Id del tipo de reserva</param>
+        /// <param name="nomb_tiporeserva">Nombre del tipo de reserva</param>
         public void Actualizar(int id_tiporeserva,string nomb_tiporeserva)
         {
             ID_TipoReserva = id_tiporeserva;
             Nombre_TipoReserva = nomb_tiporeserva;            
         }
+        
+        /// <summary>
+        ///  Metodo para Anular un Tipo de Reserva
+        /// </summary>
         public void Anular()
         {
             
