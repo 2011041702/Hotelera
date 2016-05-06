cab8c81f622c4ea1cfe91ce26e8cccc91a1a72d0
Sergio André Pérez Chávez <sergio_perezch@hotmail.com>
2016-05-06 11:19:14 -0500

    Comentando

--- Hotelera.Dominio/Proveedor.cs
+++ Hotelera.Dominio/Proveedor.cs
@@ -8,11 +8,17 @@ namespace Hotelera.Dominio
 {
     public class Proveedor : Persona
     {
-
+        /// <summary>
+        ///  Parametros de Proveedor
+        /// </summary>
         public int ID_Proveedor { get; private set; }
         public string Estado_Proveedor { get; private set; }
 
-
+        /// <summary>
+        ///  Metodo insertar un Proveedor , los demas parametros los jala de la clase Cliente
+        /// </summary>
+        /// <param name="id_pro">Id de Provedor</param>
+        /// <param name="estado_pro">Estado del Provedor</param>
         public Proveedor(int id_pro, string estado_pro, string nombre_cli, string apel_cli, string gen_cli, string tipoDoc_cli,
                                                             string nrDoc_cli, string tel_cli, string email_cli, string dir_cli) 
                                                             :base( nombre_cli, apel_cli, gen_cli, tipoDoc_cli,
@@ -21,12 +27,22 @@ namespace Hotelera.Dominio
             this.ID_Proveedor = id_pro;
             this.Estado_Proveedor = estado_pro;
         }
+        
+        /// <summary>
+        ///  Metodo para actualizar un Proveedor
+        /// </summary>
+        /// <param name="id_pro">Id de Provedor</param>
+        /// <param name="estado_pro">Estado del Provedor</param>
         public void Actualizar(int id_prov, string estado_provee)
         {
             ID_Proveedor = id_prov;
             
             Estado_Proveedor = estado_provee;
         }
+        
+        /// <summary>
+        ///  Metodo para Anular un Proveedor
+        /// </summary>
         public void Anular()
         {
             Estado_Proveedor = "INACTIVO";
