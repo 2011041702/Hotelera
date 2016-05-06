4bbb815ead8ee91f53a40776f5b74350512ab7f8
Sergio André Pérez Chávez <sergio_perezch@hotmail.com>
2016-05-06 08:58:47 -0500

    Comentando

--- Hotelera.Dominio/TipoPago.cs
+++ Hotelera.Dominio/TipoPago.cs
@@ -7,6 +7,9 @@ namespace Hotelera.Dominio
 {
     public class TipoPago
     {
+        /// <summary>
+        ///  Parametros de Tipo de Pago
+        /// </summary>
         public int ID_TipoPago { get; private set; }
         public string Nombre_TipoPago { get; private set; }
 
