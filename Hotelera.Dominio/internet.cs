a4388490aea95097b89571628563d72bf0a12f94
Sergio André Pérez Chávez <sergio_perezch@hotmail.com>
2016-05-06 08:42:15 -0500

    Comentando

--- Hotelera.Dominio/internet.cs
+++ Hotelera.Dominio/internet.cs
@@ -8,9 +8,15 @@ namespace Hotelera.Dominio
 {
     public class internet
     {
+         ///    <sumary>
+    ///     Parametros de servicio de internet
+    ///    </sumary>
         public int precio { get; private set; }
         public string descripcion { get; private set; }
 
+ ///    <sumary>
+    ///     Asignacion de costo extra de servicio de internet
+    ///    </sumary>
         public void costo()
         {
             precio = 20;
