f2e6a9ac48e6d5f5cb4ff32652e77386d62da9e4
Sergio André Pérez Chávez <sergio_perezch@hotmail.com>
2016-05-06 11:24:47 -0500

    Comentando

--- Hotelera.Dominio/Inventario.cs
+++ Hotelera.Dominio/Inventario.cs
@@ -8,10 +8,19 @@ namespace Hotelera.Dominio
 {
     public class Inventario
     {
+        
+        /// <summary>
+        ///  Parametros de la clase Inventario
+        /// </summary>
         public int ID_Inventario { get; private set; }
         public virtual Proveedor ID_Pro { get; private set; }
         public string Estado_Inventario { get; private set; }
-
+        
+        /// <summary>
+        ///  Metodo para Ingresar un Inventario
+        /// </summary>
+        /// <param name="id_prov">Id del Proveedor</param>
+        /// <param name="estado_inv">Estado del Inventario</param>
         public static Inventario Registrar(Proveedor id_prov,string estado_inv)
         {
             return new Inventario()
@@ -20,12 +29,25 @@ namespace Hotelera.Dominio
                 Estado_Inventario = estado_inv
             };
         }
+        
+        
+        /// <summary>
+        ///  Metodo para Actualizar un Inventario
+        /// </summary>
+        /// <param name="id_inven">Id del Inventario</param>
+        /// <param name="id_prov">Id del Proveedor</param>
+        /// <param name="estado_inv">Estado del Inventario</param>
         public void Actualizar(int id_inven,Proveedor id_prov, string estado_inv)
         {
             ID_Inventario = id_inven;
             ID_Pro = id_prov;
             Estado_Inventario = estado_inv;
         }
+        
+        
+        /// <summary>
+        ///  Metodo para Anular un Inventario
+        /// </summary>
         public void Anular()
         {
             Estado_Inventario = "ELIMINADO";
