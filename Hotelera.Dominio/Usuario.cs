b2ff0ed731a8a7473d61be9975a784f9f33a53ab
Sergio André Pérez Chávez <sergio_perezch@hotmail.com>
2016-05-06 08:53:01 -0500

    Comentando

--- Hotelera.Dominio/Usuario.cs
+++ Hotelera.Dominio/Usuario.cs
@@ -8,12 +8,23 @@ namespace Hotelera.Dominio
 {
     public class Usuario
     {
+        /// <summary>
+        /// Parametros de los Usuarios
+        /// </summary>
         public int ID_usu { get; private set; }
         public virtual Empleado id_Personal { get; private set; }
         public string Nombre_Usuario { get; private set; }
         public string Contraseña_Usuario { get; private set; }
         public virtual Estado_Usuario ID_Estado { get; private set; }
         public virtual Acceso_Usuario ID_AccesoUsuario { get; private set; }
+        
+         /// <summary>
+        ///  Para Registrar la informacioon de un Usuario
+        /// </summary>
+        /// <param name="id_pers">Id del Personal</param>
+        /// <param name="nomb_usu">Nombre del usuario</param>
+        /// <param name="contra_usu">Contraseña de Usuario</param>
+        /// <param name="id_estad">Id de estado para el Usuario</param>
         public static Usuario Registrar(Empleado id_pers, string nomb_usu, string contra_usu, Estado_Usuario id_estad)
         {
             return new Usuario()
@@ -24,6 +35,15 @@ namespace Hotelera.Dominio
                 ID_Estado = id_estad
             };
         }
+        
+        /// <summary>
+        ///  Para Actualizar la informacioon de un Usuario
+        /// </summary>
+        /// <param name="id_us">Id del Usuario</param>
+        /// <param name="id_pers">Id del Personal</param>
+        /// <param name="nomb_usu">Nombre del usuario</param>
+        /// <param name="contra_usu">Contraseña de Usuario</param>
+        /// <param name="id_estad">Id de estado para el Usuario</param>
         public void Actualizar(int id_us,Empleado id_pers, string nomb_usu, string contra_usu, Estado_Usuario id_estad)
         {
             ID_usu = id_us;
@@ -32,6 +52,10 @@ namespace Hotelera.Dominio
             Contraseña_Usuario = contra_usu;
             ID_Estado = id_estad;
         }
+        
+        /// <summary>
+        ///  Metodo para bloquear un Usuario
+        /// </summary>
         public void Bloquear()
         {
        
