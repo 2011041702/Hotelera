d3cfbaf573a3e7e6419abc870d2569737e23a388
Sergio André Pérez Chávez <sergio_perezch@hotmail.com>
2016-05-06 08:39:34 -0500

    Comentando

--- Hotelera.Dominio/tvcable.cs
+++ Hotelera.Dominio/tvcable.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace Hotelera.Dominio
 {
+    /// <summary>
+        ///  Parametros de tvcable
+        /// </summary>
+
     class tvcable
     {
         public int precio { get; private set; }
         public string descripcion { get; private set; }
 
+    ///    <sumary>
+    ///     Asignacion de costo extra de servicio
+    ///    </sumary>
+    
         public void costo()
         {
             precio = 5;
