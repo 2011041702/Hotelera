3a01eec68f788910bf964a94248517c6efb8c8a3
Sergio André Pérez Chávez <sergio_perezch@hotmail.com>
2016-05-06 11:30:00 -0500

    Comentando

--- Hotelera.Infraestructura/InicializadorBD.cs
+++ Hotelera.Infraestructura/InicializadorBD.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using System.Data.Entity;
 namespace Hotelera.Infraestructura
 {
+     /// <summary>
+        ///  Para hacer una conexion con nuestra BD a traves del contexto
+        /// </summary>
     public class InicializadorBD : CreateDatabaseIfNotExists<HoteleraContexto>
     {
         protected override void Seed(HoteleraContexto context)
